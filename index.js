672cb10394f4ee7716092d3ce7c2b9d3d8e101bc
Andrija Petrovic <andrija.hers@gmail.com>
2017-10-19 09:18:14 -0400

    Proper error reporting

--- index.js
+++ index.js
@@ -8,44 +8,53 @@ function createRegistryBase(execlib){
   }
   lib.inherit(RegistryBase,lib.DIContainer);
   RegistryBase.prototype.register = function (modulename, mod) {
-    var ret;
     if (typeof mod !== 'undefined') {
       if (this.get(modulename)) {
         console.error(process.pid, 'duplicate register for', modulename);
-        return;
+        return q.reject(new lib.Error('DUPLICATE_MODULE_REGISTER_ATTEMPT', 'Duplicate register for '+modulename+' is not allowed'));
       }
       lib.DIContainer.prototype.register.call(this, modulename, mod);
       return mod;
     }
     if (!this.busy(modulename)) {
-      ret = this.waitFor(modulename);
-      this.finalizeRegister(modulename);
-    } else {
-      ret = this.waitFor(modulename);
+      this.waitFor(modulename);
+      return this.finalizeRegister(modulename);
     }
-    return ret;
+    return this.waitFor(modulename);
   };
   RegistryBase.prototype.finalizeRegister = function (modulename) {
+    var d, ret;
     try {
-      this.register(modulename, require(modulename)(execlib));
+      return q(this.register(modulename, require(modulename)(execlib)));
     }
     catch(e){
       if(execlib.execSuite.installFromError){
-        execlib.execSuite.installFromError(this.registerAfterInstall.bind(this,modulename),e);
+        d = q.defer();
+        ret = d.promise;
+        execlib.execSuite.installFromError(this.registerAfterInstall.bind(this,modulename,d),e);
+        return ret;
       }else{
         console.log(e.stack);
         console.log(e);
+        return q.reject(e);
       }
     }
   };
-  RegistryBase.prototype.registerAfterInstall = function (modulename, should) {
+  RegistryBase.prototype.registerAfterInstall = function (modulename, defer, should) {
+    var e, frres;
     if (!should) {
-      var e = new lib.Error('MODULE_INSTALLATION_FAILED',modulename);
+      e = new lib.Error('MODULE_INSTALLATION_FAILED',modulename);
       e.modulename = modulename;
+      if (defer) {
+        defer.reject(e);
+      }
       this.fail(e);
       return;
     }
-    this.finalizeRegister(modulename);
+    frres = this.finalizeRegister(modulename);
+    if (defer) {
+      qlib.promise2defer(frres, defer);
+    }
   };
   RegistryBase.prototype.spawn = function (modulename, prophash) {
     var d = q.defer();
